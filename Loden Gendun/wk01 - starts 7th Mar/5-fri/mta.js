c2bdc88e91383fdf19af55b1c4d6fd6333ef8725
Loden <loden.gendun@students.mq.edu.au>
2022-03-13 08:52:29 +1100

    Week 1 (fri) Homework (refactored)

--- Loden Gendun/wk01 - starts 7th Mar/5-fri/mta.js
+++ Loden Gendun/wk01 - starts 7th Mar/5-fri/mta.js
@@ -1,77 +1,16 @@
-console.log("mta script functional");
-
-// # MTA Lab
-
-// ## Objectives:
-// * Apply your knowledge of Javascript to solve a real world problem.
-// * Get really good at array manipulation.
-
-// #### Activity
-// * Create a program that models a simple subway system.
-
-// * The program takes the line and stop that a user is getting on at and the line
-// and stop that user is getting off at and prints the journey and the total number of stops for the trip in the console:
-
-// ```javascript
-// planTrip('N', 'Times Square', '6', '33rd'); // This is only a suggested function name and signature.
-
-// // console.log() shows output similar to this:
-// // "You must travel through the following stops on the N line: 34th, 28th, 23rd, Union Square."
-// // "Change at Union Square."
-// // "Your journey continues through the following stops: 23rd, 28th, 33rd."
-// // "7 stops in total."
-// ```
-
-// * There are 3 subway lines:
-//   * The N line has the following stops: Times Square, 34th, 28th, 23rd, Union Square, and 8th
-//   * The L line has the following stops: 8th, 6th, Union Square, 3rd, and 1st
-//   * The 6 line has the following stops: Grand Central, 33rd, 28th, 23rd, Union Square, and Astor Place.
-//   * All 3 subway lines intersect at Union Square, but there are no other intersection points. (For example, this means the 28th stop on the N line is different than the 28th street stop on the 6 line, so you'll have to differentiate this when you name your stops in the arrays.)
-// * Tell the user the number of stops AND the stops IN ORDER that they will pass through or change at.
-
-
-// #### Hints:
-// * Work out how you would do it on paper first! Then start to explain that process in Javascript.
-// * Get the program to work for a single line before trying to tackle multiple lines.
-// * Don't worry about prompting the user for input. Hard code some values to get it working. You can use ```prompt()``` later to make it more interactive.
-// * Consider diagramming the lines by sketching out the subway lines and their stops and intersection.
-// * The key to the lab is finding the index positions of each stop. (hint: ```indexOf()```)
-// * Make sure the stops that are the same for different lines have different names (i.e. 23rd on the N and on the 6 need to be differentiated)
-
-// SUBWAY object
+// SUBWAY object holds line arrays of stops.
 const subway = {
-    // ARRAYS of stops
-    N: [
-        'Times Square', 
-        '34th', 
-        '28th', 
-        '23rd', 
-        'Union Square',
-        '8th'
-    ],
-    L: [
-        '8th', 
-        '6th', 
-        'Union Square', 
-        '3rd',
-        '1st'
-    ],
-    '6': [
-        'Grand Central', 
-        '33rd', 
-        '28th', 
-        '23rd', 
-        'Union Square',
-        'Astor Place'
-    ]
+    N: ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'],
+    L: ['8th', '6th', 'Union Square', '3rd', '1st'],
+    '6': ['Grand Central', '33rd', '28th', '23rd', 'Union Square', 'Astor Place']
 };
 
-// HELPER FUNCTIONS
+// HELPER FUNCTIONS---------------------------------------------------------------------------
 const changeRequired = function(lineAt, lineDest) {
-    return lineAt != lineDest; // return true if commuter not at final destination
+    return lineAt != lineDest; // return true if commuter not on the line of their destination
 };
 
-// vector function will determine whether iterator increments or decrements
+// Determines whether iterator increments or decrements
 const direction = function(commuter, nextStop) {
     const line = commuter.line;
     if (subway[line].indexOf(commuter.position) < subway[line].indexOf(nextStop)) {
@@ -80,6 +19,7 @@ const direction = function(commuter, nextStop) {
     return -1;
 };
 
+// Determines whether commuter has reached their next stop.
 const tripOver = function(i, commuter, nextStop) {
     const line = commuter.line;
     if (direction(commuter, nextStop) > 0) {
@@ -88,31 +28,44 @@ const tripOver = function(i, commuter, nextStop) {
     return i < subway[line].indexOf(nextStop);
 };
 
+// Determines if commuter has reached their destination.
+let destinationReached = function(commuter, lineOff, stopOff) {
+    return commuter.line === lineOff && commuter.position === stopOff;
+};
+//--------------------------------------------------------------------------------------------
 
-// MAIN FUNCTION
+// MAIN FUNCTION--------------------------------------------------------------------------------------------------
 const planTrip = function(lineOn, stopOn, lineOff, stopOff) {
     
-    let stopsToTravel;
+    // Variables
+    let changeOccured = false;
     let stopCount = 0;
+    let stopsToTravel;
+    let nextStop;
     
-    const travel = function(commuter, nextStop) {
-        // Set new trip
-        stopsToTravel = [];
-    
-        const line = commuter.line;
-        for (let i = subway[line].indexOf(commuter.position) + direction(commuter, nextStop); !tripOver(i, commuter, nextStop); i = i + direction(commuter, nextStop)) {
-            commuter.position = subway[line][i];
-            stopsToTravel.push(commuter.position);
-            stopCount ++;
-        };
-    };
-    
-    // object keeps tracks of commuters line and position
+    // Keeps tracks of commuters line and position.
     let commuter = { 
         line: lineOn,
         position: stopOn
     };
-    let nextStop;
+
+    // Travel function moves commuter along train line to their next stop. 
+    const travel = function(commuter, nextStop) {
+        stopsToTravel = []; // Set new trip
+    
+        const line = commuter.line;
+        const startPoint = subway[line].indexOf(commuter.position) + direction(commuter, nextStop)
+
+        for (let i = startPoint; !tripOver(i, commuter, nextStop); i = i + direction(commuter, nextStop)) {
+            commuter.position = subway[line][i];
+            stopsToTravel.push(commuter.position);
+        };
+
+        stopCount += stopsToTravel.length; // Add stops travelled to stopCount.
+
+        stopsToTravel = stopsToTravel.join(', '); // Convert stopsToTravel array to string for output.
+    };
+    
     // check if destination is on another line
     if (changeRequired(lineOn, lineOff)) {
         nextStop = 'Union Square';
@@ -120,25 +73,24 @@ const planTrip = function(lineOn, stopOn, lineOff, stopOff) {
         nextStop = stopOff;
     };
 
-    let changeOccured = false;
+    // Travel until commuter has reached their destination.
+    while(!destinationReached(commuter, lineOff, stopOff)) {
 
-    // travel until commuter is at the line and stop they need to be at.
-    while(commuter.line != lineOff || commuter.position != stopOff) {
-        
+        // Moves commuter from current position to their next stop.
         travel(commuter, nextStop);
         
-        if (changeOccured) {
-            const output = `Your journey continues through the following stops: ${stopsToTravel.join(', ')}.`;
-            console.log(output);
-        } else {
-            const output = `You must travel through the following stops on the ${lineOn} line: ${stopsToTravel.join(', ')}.`;
+        if (!changeOccured) {
+            const output = `You must travel through the following stops on the ${lineOn} line: ${stopsToTravel}.`;
             console.log(output);
             if (changeRequired(lineOn, lineOff)) {
                 console.log("Change at Union Square.");
             };
+        } else {
+            const output = `Your journey continues through the following stops: ${stopsToTravel}.`;
+            console.log(output);
         };
         
-        // Change lines
+        // Conditional for changing lines.
         if(commuter.line != lineOff) {
             commuter.line = lineOff;
             changeOccured = true;
@@ -149,3 +101,4 @@ const planTrip = function(lineOn, stopOn, lineOff, stopOff) {
     };
     console.log(`${stopCount} stops in total.`);
 };
+//----------------------------------------------------------------------------------------------------------------
