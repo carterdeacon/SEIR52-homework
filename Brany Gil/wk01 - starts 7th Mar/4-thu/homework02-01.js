35f9a177b3d34ed2793dc82c40f56d2ac0543d9b
Brany Flores Gil <branyfloresgil@Branys-MacBook-Pro.local>
2022-03-13 20:12:59 +1100

    10/homework

---
+++ Brany Gil/wk01 - starts 7th Mar/4-thu/homework02-01.js
@@ -0,0 +1,19 @@
+// The Cash Register
+// Write a function called cashRegister that takes a shopping cart object. The object contains item names and prices (itemName: itemPrice). The function should return the total price of the shopping cart. Example
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Output
+// cashRegister(cartForParty)); // 60.55
