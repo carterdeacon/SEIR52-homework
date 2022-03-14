3bdc5eb46f6676d1ae5c1660a404c27981911872
Kate Hunter <iamkatehunter@gmail.com>
2022-03-14 20:57:00 +1100

    MTA aka I know more than I did Tuesday

---
+++ Kate Hunter/wk01 - starts 7th Mar/5-fri/js/mta.js
@@ -0,0 +1,49 @@
+
+console.log("Far out")
+
+
+// Take the line and stop that a user is getting on at and the line and stop that user is getting off at and prints the journey and the total number of stops for the trip in the console.
+
+
+// Subway lines
+
+const subwayLines = {
+    n: [ 'Times Square', '34th', 'n28th', 'n23rd', 'Union Square', '8th' ],
+    l: [ '8th', '6th', 'Union Square', '3rd', '1st' ],
+    six: ['Grand Central', '33rd', 'six28th', 'six23rd', 'Union Square', 'Astor Place'],
+}
+
+// Microwave
+const planTrip = function (onLine, onStop, offLine, offStop) {
+    for (let i = indexOf(onStop); < subwayLines.n.length; i++) 
+    console.log('You will travel through ' + ${onStop}++ ", " + ${OnStop}+2 + ',' )
+
+       else if (let i = indexOf(onStop); < subwayLines.l.length; i++) 
+       console.log('You will travel through ' + ${onStop}++ ", " + ${OnStop}+2 + ',' )
+
+       else if (let i = indexOf(onStop); < subwayLines.six.length; i++) 
+       console.log('You will travel through ' + ${onStop}++ ", " + ${OnStop}+2 + ',' )
+}
+        
+
+        // if start point is nTimesSquare, and end point is 6GrandCentral, then count number of stations between start and end. Then name stations between start and end. 
+
+        
+    
+    console.log(``)
+    // You will travel through a, b, c, d, change at Union Square, then travel through e before getting off at f.
+
+    
+
+
+//Subway lines in the Microwave
+planTrip('N', 'Times Square', 'Six', 'Grand Central')
+
+
+indexOf()
+.includes()
+
+
+
+
+//indexOf(nLine)
