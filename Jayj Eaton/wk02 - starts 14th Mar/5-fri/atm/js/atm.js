5b24b9c0671e4412fe226cb4f93689b32c199a3c
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-03-18 16:28:44 +0800

    basic hw needs cross filters

---
+++ Jayj Eaton/wk02 - starts 14th Mar/5-fri/atm/js/atm.js
@@ -0,0 +1,70 @@
+let checkingTotal = 0;
+let savingsTotal = 0;
+let checkingInput = 0;
+let savingsInput = 0;
+let $checkingBalance = $("#checking-balance");
+let $savingsBalance = $("#savings-balance");
+
+// ---- basic add and subtractions is done just need to add ways to pull from other accounts and set balance to zero in current account
+//
+// --- checking deposit
+$("#checking-deposit").click(function () {
+	checkingInput = Number($("#checking-amount").val());
+	checkingTotal += checkingInput;
+	$checkingBalance.text("$" + checkingTotal);
+	if (checkingTotal >= 1) {
+		$checkingBalance.removeClass("zero");
+	}
+	return checkingTotal;
+});
+// --- checking withdraw
+$("#checking-withdraw").click(function () {
+	// --- this line fucked me... it needed to be outside the loop to read the value of input before it ran
+	checkingInput = Number($("#checking-amount").val());
+	// --- if balance is higher then withdraw amount
+	if (checkingTotal >= checkingInput) {
+		checkingTotal -= checkingInput;
+		$checkingBalance.text("$" + checkingTotal);
+		// --- if total is $0 addback red class
+		if (checkingTotal === 0) {
+			$checkingBalance.addClass("zero");
+		}
+	}
+});
+
+//--- savings deposit
+$("#savings-deposit").click(function () {
+	savingsInput = Number($("#savings-amount").val());
+	savingsTotal += savingsInput;
+	$savingsBalance.text("$" + savingsTotal);
+	if (savingsTotal >= 1) {
+		$savingsBalance.removeClass("zero");
+	}
+});
+
+// --- savings withdraw
+$("#savings-withdraw").click(function () {
+	savingsInput = Number($("#savings-amount").val());
+	//--- if balance is higher then withdraw amount
+	if (savingsTotal > -1 + savingsInput) {
+		savingsTotal -= savingsInput;
+		$savingsBalance.text("$" + savingsTotal);
+		if (savingsTotal === 0) {
+			$savingsBalance.addClass("zero");
+		}
+	}
+});
+if (checkingTotal === 0) {
+	$checkingBalance.addClass("zero");
+}
+if (savingsTotal === 0) {
+	$savingsBalance.addClass("zero");
+}
+
+// * What happens when the user wants to withdraw more money from the checking
+// account than is in the account? These accounts have overdraft protection, so if
+// a withdrawal can be covered by the balances in both accounts, take the checking
+// balance down to $0 and take the rest of the withdrawal from the savings account.
+// If the withdrawal amount is more than the combined account balance, ignore it.
+// * Make sure there is overdraft protection going both ways.
+// * Are there ways to refactor your code to make it DRYer?
