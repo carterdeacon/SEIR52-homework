c00f43273589f0aa026c92f65a8737f3f70cf007
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-03-19 08:45:13 +0800

    somecleaning

--- Jayj Eaton/wk02 - starts 14th Mar/5-fri/atm/js/atm.js
+++ Jayj Eaton/wk02 - starts 14th Mar/5-fri/atm/js/atm.js
@@ -5,8 +5,6 @@ let savingsInput = 0;
 let $checkingBalance = $("#checking-balance");
 let $savingsBalance = $("#savings-balance");
 
-// ---- basic add and subtractions is done just need to add ways to pull from other accounts and set balance to zero in current account
-//
 // --- checking deposit
 $("#checking-deposit").click(function () {
 	checkingInput = Number($("#checking-amount").val());
@@ -15,11 +13,11 @@ $("#checking-deposit").click(function () {
 	if (checkingTotal >= 1) {
 		$checkingBalance.removeClass("zero");
 	}
-	return checkingTotal;
+	console.log("$checkingBalance.val()");
 });
 // --- checking withdraw
 $("#checking-withdraw").click(function () {
-	// --- this line fucked me... it needed to be outside the loop to read the value of input before it ran
+	// --- this line fucked me... it needed to be outside the loop to read the value
 	checkingInput = Number($("#checking-amount").val());
 	// --- if balance is higher then withdraw amount
 	if (checkingTotal >= checkingInput) {
@@ -28,7 +26,7 @@ $("#checking-withdraw").click(function () {
 		// --- if total is $0 addback red class
 		if (checkingTotal === 0) {
 			$checkingBalance.addClass("zero");
-		}
+		} // --- overdraft ---
 	} else if (checkingTotal + savingsTotal >= checkingInput) {
 		let overdraft = checkingInput - checkingTotal;
 		checkingTotal = 0;
@@ -36,9 +34,11 @@ $("#checking-withdraw").click(function () {
 		savingsTotal -= overdraft;
 		$savingsBalance.text("$" + savingsTotal);
 		console.log("overdraft protection activated");
+		if (checkingTotal === 0) {
+			$checkingBalance.addClass("zero");
+		}
 	}
 });
-
 //--- savings deposit
 $("#savings-deposit").click(function () {
 	savingsInput = Number($("#savings-amount").val());
@@ -48,7 +48,6 @@ $("#savings-deposit").click(function () {
 		$savingsBalance.removeClass("zero");
 	}
 });
-
 // --- savings withdraw
 $("#savings-withdraw").click(function () {
 	savingsInput = Number($("#savings-amount").val());
@@ -58,7 +57,7 @@ $("#savings-withdraw").click(function () {
 		$savingsBalance.text("$" + savingsTotal);
 		if (savingsTotal === 0) {
 			$savingsBalance.addClass("zero");
-		}
+		} /// --- overdraft ---
 	} else if (checkingTotal + savingsTotal >= checkingInput) {
 		let overdraft = savingsInput - savingsTotal;
 		savingsTotal = 0;
@@ -66,19 +65,8 @@ $("#savings-withdraw").click(function () {
 		checkingTotal -= overdraft;
 		$checkingBalance.text("$" + checkingTotal);
 		console.log("overdraft protection activated");
+		if (savingsTotal === 0) {
+			$savingsBalance.addClass("zero");
+		}
 	}
 });
-if (checkingTotal === 0) {
-	$checkingBalance.addClass("zero");
-}
-if (savingsTotal === 0) {
-	$savingsBalance.addClass("zero");
-}
-
-// * What happens when the user wants to withdraw more money from the checking
-// account than is in the account? These accounts have overdraft protection, so if
-// a withdrawal can be covered by the balances in both accounts, take the checking
-// balance down to $0 and take the rest of the withdrawal from the savings account.
-// If the withdrawal amount is more than the combined account balance, ignore it.
-// * Make sure there is overdraft protection going both ways.
-// * Are there ways to refactor your code to make it DRYer?
