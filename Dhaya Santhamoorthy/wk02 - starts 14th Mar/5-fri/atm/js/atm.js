17be4e79f9800f680b785537252c4c0ae8bce3b5
Dhayananth Santhamoorthy <dhaya@192-168-1-117.tpgi.com.au>
2022-03-19 19:18:18 +1100

    Fixed a typo in a variable name

--- Dhaya Santhamoorthy/wk02 - starts 14th Mar/5-fri/atm/js/atm.js
+++ Dhaya Santhamoorthy/wk02 - starts 14th Mar/5-fri/atm/js/atm.js
@@ -15,11 +15,11 @@ $('#savings-deposit').on('click', function () {
     $('#savings-amount').val("");                                                   // Clearing the input field for balance amount
 });
 
-const manageDeposit = function ( iDOfAccount, depositValue ) {                     // Helper function to manage deposits
-    let currBalance = Number( $(iDOfAccount).text().substring(1) );                // To extract only tne number portion of the balance removing the "$" symbol.
+const manageDeposit = function ( idOfAccount, depositValue ) {                     // Helper function to manage deposits
+    let currBalance = Number( $(idOfAccount).text().substring(1) );                // To extract only tne number portion of the balance removing the "$" symbol.
     currBalance += depositValue;
-    $(iDOfAccount).text( '$' +  currBalance  );
-    setAccountColor( iDOfAccount, currBalance );                                   // function call to set account color                                            
+    $(idOfAccount).text( '$' +  currBalance  );
+    setAccountColor( idOfAccount, currBalance );                                   // function call to set account color                                            
 };
 
 $('#checking-withdraw').on('click', function () {    
@@ -38,32 +38,32 @@ $('#savings-withdraw').on('click', function () {
     $('#savings-amount').val("");    
 });
 
-const manageWithdraw = function ( iDOfPrimaryAccount, iDOfSecondaryAccount, withDrawValue ) {            // Helper function to manage withdrawals    
-    let primaryAccountBalance = Number( $(iDOfPrimaryAccount).text().substring(1) );
-    let SecondaryAccountBalance = Number( $(iDOfSecondaryAccount).text().substring(1) );
+const manageWithdraw = function ( idOfPrimaryAccount, idOfSecondaryAccount, withDrawValue ) {            // Helper function to manage withdrawals    
+    let primaryAccountBalance = Number( $(idOfPrimaryAccount).text().substring(1) );
+    let SecondaryAccountBalance = Number( $(idOfSecondaryAccount).text().substring(1) );
 
     if ( primaryAccountBalance >= withDrawValue ) {
         primaryAccountBalance -= withDrawValue;
-        $(iDOfPrimaryAccount).text( '$' +  primaryAccountBalance  );
+        $(idOfPrimaryAccount).text( '$' +  primaryAccountBalance  );
     } else {
         let totalBalance = primaryAccountBalance + SecondaryAccountBalance;
         if ( totalBalance  >= withDrawValue ) {
             totalBalance -= withDrawValue;
             primaryAccountBalance = 0;
             SecondaryAccountBalance = totalBalance;
-            $(iDOfPrimaryAccount).text( '$' +  primaryAccountBalance  );
-            $(iDOfSecondaryAccount).text( '$' +  SecondaryAccountBalance  );
+            $(idOfPrimaryAccount).text( '$' +  primaryAccountBalance  );
+            $(idOfSecondaryAccount).text( '$' +  SecondaryAccountBalance  );
         }        
     }  
 
-    setAccountColor( iDOfPrimaryAccount, primaryAccountBalance );                        //  function call to set primary account color  
-    setAccountColor( iDOfSecondaryAccount, SecondaryAccountBalance );                    // function call to set secondary account color  
+    setAccountColor( idOfPrimaryAccount, primaryAccountBalance );                        //  function call to set primary account color  
+    setAccountColor( idOfSecondaryAccount, SecondaryAccountBalance );                    // function call to set secondary account color  
 };
 
-const setAccountColor =  function ( iDOfAccount, balance ) {                        // Helper function to set the account color
+const setAccountColor =  function ( idOfAccount, balance ) {                        // Helper function to set the account color
     if ( balance === 0 ) {
-        $(iDOfAccount).addClass('zero');
+        $(idOfAccount).addClass('zero');
     } else {
-        $(iDOfAccount).removeClass('zero');
+        $(idOfAccount).removeClass('zero');
     }
 };
