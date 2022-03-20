4ab1c37186934cc9b52f368e5a751c41ad3a3af7
Jaque Elizandro <jaque@elizandro.com.br>
2022-03-20 19:24:23 +1100

    change the name of the variable totalChecking -> checkingBalance

--- Jaqueline Elizandro/wk02 - starts 14th Mar/5-fri/atm/js/atm.js
+++ Jaqueline Elizandro/wk02 - starts 14th Mar/5-fri/atm/js/atm.js
@@ -1,16 +1,16 @@
 
-let totalChecking = 0; // store the current value of the checking account
-let totalSavings = 0; // store the current value of the savings account
+let checkingBalance = 0; // store the current value of the checking account
+let savingsBalance = 0; // store the current value of the savings account
 
-const updateDisplay = function (totalChecking, totalSavings) { // change the balance value and colour on display
-    $('#checking-balance').text('$' + totalChecking)
-    $('#savings-balance').text('$' + totalSavings)
-    if (totalChecking === 0) {
+const updateDisplay = function (checkingBalance, savingsBalance) { // change the balance value and colour on display
+    $('#checking-balance').text('$' + checkingBalance)
+    $('#savings-balance').text('$' + savingsBalance)
+    if (checkingBalance === 0) {
         $('#checking div').toggleClass('zero', true);
     } else {
         $('#checking div').toggleClass('zero', false);
     }
-    if (totalSavings === 0) {
+    if (savingsBalance === 0) {
         $('#savings div').toggleClass('zero', true);
     } else {
         $('#savings div').toggleClass('zero', false);
@@ -18,33 +18,33 @@ const updateDisplay = function (totalChecking, totalSavings) { // change the bal
 };
 
 $('#checking-deposit').click(function () { // update the balance value at checkings deposit
-    totalChecking += + $('#checking-amount').val();
-    updateDisplay(totalChecking, totalSavings);
+    checkingBalance += + $('#checking-amount').val();
+    updateDisplay(checkingBalance, savingsBalance);
 });
 
 $('#savings-deposit').click(function () { // update the balance value at savings deposit
-    totalSavings += + $('#savings-amount').val();
-    updateDisplay(totalChecking, totalSavings);
+    savingsBalance += + $('#savings-amount').val();
+    updateDisplay(checkingBalance, savingsBalance);
 });
 
 $('#checking-withdraw').click(function () { // update the balance value at checkings withdraw
     const amountWithdrawChecking = + $('#checking-amount').val();
-    if (amountWithdrawChecking <= totalChecking) {
-        totalChecking -= amountWithdrawChecking
-    } else if (amountWithdrawChecking <= (totalChecking + totalSavings)) {
-        totalSavings -= (amountWithdrawChecking - totalChecking);
-        totalChecking = 0;
+    if (amountWithdrawChecking <= checkingBalance) {
+        checkingBalance -= amountWithdrawChecking
+    } else if (amountWithdrawChecking <= (checkingBalance + savingsBalance)) {
+        savingsBalance -= (amountWithdrawChecking - checkingBalance);
+        checkingBalance = 0;
     };
-    updateDisplay(totalChecking, totalSavings);
+    updateDisplay(checkingBalance, savingsBalance);
 });
 
 $('#savings-withdraw').click(function () { // update the balance value at savings withdraw
     const amountWithdrawSavings = + $('#savings-amount').val();
-    if (amountWithdrawSavings <= totalSavings) {
-        totalSavings -= amountWithdrawSavings
-    } else if (amountWithdrawSavings <= (totalSavings + totalChecking)) {
-        totalChecking -= (amountWithdrawSavings - totalSavings);
-        totalSavings = 0;
+    if (amountWithdrawSavings <= savingsBalance) {
+        savingsBalance -= amountWithdrawSavings
+    } else if (amountWithdrawSavings <= (savingsBalance + checkingBalance)) {
+        checkingBalance -= (amountWithdrawSavings - savingsBalance);
+        savingsBalance = 0;
     };
-    updateDisplay(totalChecking, totalSavings);
+    updateDisplay(checkingBalance, savingsBalance);
 });
