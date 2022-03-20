07b5d34d11684afb19da7d60b73fbc17dedbd343
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-03-20 20:16:24 +0800

    the dryest

--- Jayj Eaton/wk02 - starts 14th Mar/5-fri/atm/js/atm.js
+++ Jayj Eaton/wk02 - starts 14th Mar/5-fri/atm/js/atm.js
@@ -13,15 +13,14 @@ $("#checking-deposit").click(function () {
 });
 // --- checking withdraw
 $("#checking-withdraw").click(function () {
-	// --- this line fucked me... it needed to be outside the loop to read the value
 	checkingInput = Number($("#checking-amount").val());
 	// --- if balance is higher then input amount
 	if (checkingTotal >= checkingInput) {
 		checkingTotal -= checkingInput;
 		$checkingBalance.text("$" + checkingTotal);
 		toggleClass(checkingTotal, savingsTotal);
-		// --- overdraft ---
 	} else if (checkingTotal + savingsTotal >= checkingInput) {
+		// --- overdraft ---
 		let overdraft = checkingInput - checkingTotal;
 		checkingTotal = 0;
 		$checkingBalance.text("$" + checkingTotal);
@@ -40,13 +39,12 @@ $("#savings-deposit").click(function () {
 // --- savings withdraw
 $("#savings-withdraw").click(function () {
 	savingsInput = Number($("#savings-amount").val());
-	//--- if balance is higher then input amount
 	if (savingsTotal > -1 + savingsInput) {
 		savingsTotal -= savingsInput;
 		$savingsBalance.text("$" + savingsTotal);
 		toggleClass(checkingTotal, savingsTotal);
-		/// --- overdraft ---
 	} else if (checkingTotal + savingsTotal >= checkingInput) {
+		// --- overdraft ---
 		let overdraft = savingsInput - savingsTotal;
 		savingsTotal = 0;
 		$savingsBalance.text("$" + savingsTotal);
@@ -55,7 +53,7 @@ $("#savings-withdraw").click(function () {
 		console.log("overdraft protection activated");
 		toggleClass(checkingTotal, savingsTotal);
 	}
-});
+}); // --- toggles red background if 0 in accounts ---
 const toggleClass = function (checkingTotal, savingsTotal) {
 	if (checkingTotal === 0 ? $checkingBalance.addClass("zero") : $checkingBalance.removeClass("zero"));
 	if (savingsTotal === 0 ? $savingsBalance.addClass("zero") : $savingsBalance.removeClass("zero"));
