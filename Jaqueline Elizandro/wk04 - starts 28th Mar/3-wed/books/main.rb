96397954e051a9d34c96bba30e06b37db6268845
Jaque Elizandro <jaque@elizandro.com.br>
2022-03-31 10:17:56 +1100

    add variable @volume_info

--- Jaqueline Elizandro/wk04 - starts 28th Mar/3-wed/books/main.rb
+++ Jaqueline Elizandro/wk04 - starts 28th Mar/3-wed/books/main.rb
@@ -9,6 +9,7 @@ end
 get '/display' do
     title = params[:book_title]
     book_url = "https://www.googleapis.com/books/v1/volumes?q=title:#{title}"
-    @info_book = HTTParty.get book_url
+    info_book = HTTParty.get book_url
+    @volume_info = info_book["items"][0]["volumeInfo"]
     erb :display
 end
