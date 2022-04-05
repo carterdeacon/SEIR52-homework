83c240d179d534a1dcc12bbf3311324e6b6dc2da
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-04-06 06:57:17 +0800

    mountain homework

--- Jayj Eaton/wk05 - starts 4th Apr/2-tue/earth-pointy-bits/config/routes.rb
+++ Jayj Eaton/wk05 - starts 4th Apr/2-tue/earth-pointy-bits/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  get '/mountains' => 'mountains#index'
+  get '/mountains/new' => 'mountains#new', :as => :new_mountain
+  post '/mountains' => 'mountains#create'
+  get '/mountains/:id' => 'mountains#show', :as => :mountain
+  get '/mountains/:id/edit' => 'mountains#edit', :as => :edit_mountain
+  post '/mountains/:id' => 'mountains#update'
+  get '/mountains/:id/delete' => 'mountains#destroy', :as => :delete_mountain
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
