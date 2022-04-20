f3c32a818c9e39bdeee89a7d0cd543366e4c2aa9
Jaque Elizandro <jaque@elizandro.com.br>
2022-04-20 18:32:13 +1000

    book api using AJAX

---
+++ Jaqueline Elizandro/wk07 - starts 18th Apr/3-wed/books/js/books.js
@@ -0,0 +1,20 @@
+const fetchFact = function() {
+    const xhr = new XMLHttpRequest();
+    const search = document.getElementById("search").value;
+    xhr.open('GET', `https://www.googleapis.com/books/v1/volumes?q=title:${search}`);
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== 4) {
+            return;
+        }
+        const data = JSON.parse(xhr.responseText);
+        data.items.forEach(function(book) {
+            const img = document.createElement('img');
+            img.src = (book).volumeInfo.imageLinks.smallThumbnail;
+            document.body.appendChild(img);
+        });
+        
+    };
+};
+
+document.getElementById('fetch').addEventListener('click', fetchFact);
