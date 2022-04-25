e24e959f1c84d43e0e5cb0d2c084f56e1e3af573
ebeecroft1 <eric.beecroft@gmail.com>
2022-04-25 10:53:20 +1000

    Previous hw updates

--- Eric Beecroft/wk07 - starts 18th Apr/3-wed/books/js/books.js
+++ Eric Beecroft/wk07 - starts 18th Apr/3-wed/books/js/books.js
@@ -1,26 +1,51 @@
-const getCover = function () {
-    const title = document.getElementById("title").value;
+// My solution
 
+// const getCover = function () {
+//     const title = document.getElementById("title").value;
+
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=title:'+title);
+//     xhr.send();
+
+//     xhr.onreadystatechange = function () {
+//         if (xhr.readyState !== 4) {
+//             return;
+//         }
+
+//         const data = JSON.parse(xhr.responseText);
+
+//         const p = document.createElement('p');
+//         p.innerHTML = data.items[0].volumeInfo.title;
+//         document.body.appendChild(p);
+
+//         const img = document.createElement('img');
+//         img.src = data.items[0].volumeInfo.imageLinks.thumbnail;
+//         document.body.appendChild(img);
+
+//     };
+// };
+
+// document.getElementById('submit').addEventListener('click', getCover);
+
+// Joel's Solution
+const fetchCover = function (event) {
+    event.preventDefault(); // Stay on this page and don't default reload
+
+    const title = document.getElementById('title').value;
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=title:'+title);
-    xhr.send();
 
+    // open the connection to Google Books
+    xhr.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=title:' + title);
+    xhr.send(); // takes time - asynchronous
+    console.log(xhr.responseText); // empty string
     xhr.onreadystatechange = function () {
-        if (xhr.readyState !== 4) {
-            return;
-        }
-
+        if (xhr.readyState !== 4) return; // No curlies - get out of the function quickly
         const data = JSON.parse(xhr.responseText);
-
-        const p = document.createElement('p');
-        p.innerHTML = data.items[0].volumeInfo.title;
-        document.body.appendChild(p);
-
-        const img = document.createElement('img');
-        img.src = data.items[1].volumeInfo.imageLinks.thumbnail;
-        document.body.appendChild(img);
-
+        const cover = data.items[0].volumeInfo.imageLinks.thumbnail;
+        document.getElementById('cover').setAttribute('src', cover);
     };
 };
 
-document.getElementById('submit').addEventListener('click', getCover);
+const form = document.getElementById('search');
+
+form.addEventListener('submit', fetchCover);
