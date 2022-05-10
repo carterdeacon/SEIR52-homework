e7e6a50ca6a04b84f70fe42db0db8d24723335b4
Phillip Watson <phillwatson@Phillips-MacBook-Pro.local>
2022-05-10 22:42:27 +1000

    quick sort fixed

--- Phillip Watson/wk10 - starts 9th May/2-tue/divide-and-conquer-sort-exercises/test/QuickSortTests.js
+++ Phillip Watson/wk10 - starts 9th May/2-tue/divide-and-conquer-sort-exercises/test/QuickSortTests.js
@@ -1,5 +1,5 @@
 const expect  = require('chai').expect;
-const quickSort = require('../MergeSort');
+const quickSort = require('../quickSort');
 
 describe('Quick Sort', ()=>{
     it('should sort the array', ()=>{
