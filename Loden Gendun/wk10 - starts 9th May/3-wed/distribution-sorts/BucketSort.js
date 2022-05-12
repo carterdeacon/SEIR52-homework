9ce18b98cfa54ba09b1168c572f2f463bca8ecf1
Loden <loden.gendun@students.mq.edu.au>
2022-05-12 13:53:33 +1000

    atomic assignment wasn't working, fixed premature loop end

--- Loden Gendun/wk10 - starts 9th May/3-wed/distribution-sorts/BucketSort.js
+++ Loden Gendun/wk10 - starts 9th May/3-wed/distribution-sorts/BucketSort.js
@@ -1,3 +1,12 @@
+const data = [
+  241, 511, 553, 775, 361, 232, 146, 836, 650,
+  818, 520, 459, 413, 985, 967, 954, 274, 173,
+  645, 503, 813, 377, 198, 228, 750, 714, 214,
+  408, 707, 184, 317, 890, 623, 488, 856,  44,
+  94, 793, 541, 244,  49, 830, 147, 370, 785,
+  81, 605, 690, 207, 482
+];
+
 function bucketSort(items) {
   // prepare buckets
   const bucketCount = Math.ceil(Math.sqrt(items.length));
@@ -20,35 +29,39 @@ function bucketSort(items) {
   items.forEach( item => {
     // largest item breaks formula so push it in individually
     if (item !== largest) {
-      buckets[Math.floor((item-smallest)/bucketSpan)] .push(item);
+      buckets[Math.floor((item-smallest)/bucketSpan)].push(item);
     } else {
       buckets[bucketCount-1].push(largest);
     }
   });
-
+  console.log("buckets:", buckets);
   // insertion sort each bucket
   function insertionSort (array) {
     for (let i = 1; i < array.length; i++) {
       
       if (array[i] < array[i-1]) {
         for (let j = i; j > 0; j--) {
-          
           if (array[j] > array[j-1]) {
             break
           }
-          [array[j], array[j-1]] = [array[j-1], array[i]];
+          const temp = array[j];
+          array[j] = array[j-1];
+          array[j-1] = temp;
         }
         
       }
     }
   }
-  for (let i = 0; i < bucketCount-1; i++) {
+  for (let i = 0; i < bucketCount; i++) {
     insertionSort(buckets[i]);
   }
-
+  
+  console.log("sorted buckets:", buckets);
   // join buckets
   items = buckets.flat();
   return items;
 }
+console.log("data to sort:", data);
+console.log("sorted data:", bucketSort(data));
 
 module.exports = bucketSort;
